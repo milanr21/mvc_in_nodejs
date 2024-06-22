ce8f4e7c9bf36f8bfed43051206dd8facc5b51e9
milanr21 <milanrawal309@gmail.com>
2024-06-22 23:02:39 +0545

    employee models

--- models/employee.model.js
+++ models/employee.model.js
@@ -1,10 +1,26 @@
 import mongoose from 'mongoose';
 
-const employeeSchema = mongoose.Schema(
-  {},
-  {
-    timestamps: true,
-  }
-);
+const employeeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+    min: [10, 'Phone Number must be at least 10 characters'],
+  },
+});
 
 export const Employee = new mongoose.model('Employee', employeeSchema);
