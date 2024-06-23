5bd26ea82b4765c65271292039873fa6ec35bf9a
milanr21 <milanrawal309@gmail.com>
2024-06-23 19:59:37 +0545

    views using pug

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
+
+app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-var tagline = 'No more programming concept is complete without this man';
-
-app.get('/register', (req, res) => {
-  tagline: tagline;
-  res.render('register');
+app.get('/login', (req, res) => {
+  res.render('login');
 });
+
+// app.get('/register', (req, res) => {
+//   res.render('register');
+// });
 
 app.listen(port, () => {
   console.log(`The app ls listening to the port ${port}`);
